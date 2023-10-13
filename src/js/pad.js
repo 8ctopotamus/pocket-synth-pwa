d4cae14f9a93c4b35e2a30ded2711f609c8b56c9
Josh <zylo.codes@gmail.com>
2023-10-12 20:26:01 -0500

    pad

--- src/js/pad.js
+++ src/js/pad.js
@@ -9,8 +9,39 @@ export class Pad {
     this.pad = document.getElementById('pad')
     this.puck = document.getElementById('puck')
     this.setPuckStyle()
+    // attach listeners
+    this.pad.addEventListener('mousemove', this.handleMouseMove.bind(this))
+    this.pad.addEventListener('mousedown', this.handleMousedown.bind(this))
+    this.pad.addEventListener('mouseup', this.handleMouseup.bind(this))
+    this.pad.addEventListener('mouseleave', this.handleMouseLeave.bind(this))
   }
 
+  handleMouseMove(e) {
+    if (e.buttons) {
+      this.updatePuckPosition(e)
+    }
+  }
+
+  handleMousedown(e) {
+    this.updatePuckPosition(e)
+    this.pressed = true
+  }
+  
+  handleMouseup(e) {
+    this.updatePuckPosition(e)
+    this.pressed = false
+  }
+  
+  handleMouseLeave(e) {
+    this.pressed = false
+  }
+
+  updatePuckPosition(e) {
+    this.xPosition = this.clamp(e.offsetX / this.pad.offsetWidth)
+    this.yPosition = this.clamp(e.offsetY / this.pad.offsetHeight)
+    this.setPuckStyle()
+  }
+  
   clamp(val) {
     return Math.min(Math.max(val, 0), 1)
   }
@@ -21,5 +52,4 @@ export class Pad {
     this.puck.style.left = leftPercent
     this.puck.style.top = topPercent
   }
-
 }
